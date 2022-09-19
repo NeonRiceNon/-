598921f3228fd412f1f79c524faacc832f80513e
Neon Rice <112954242+NeonRiceNon@users.noreply.github.com>
2022-09-19 19:27:14 +0300

    zad_2.3

---
+++ zad_2.3/src/com/company/Basket.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Basket {
+
+        public  void f( ){
+        String arr_kyp[] = new String[5];
+        arr_kyp[0] = "apple";
+        arr_kyp[1] = "apple";
+        arr_kyp[2] = "apple";
+        arr_kyp[3] = "milk";
+        arr_kyp[4] = "milk";
+
+    }
+
+}
---
+++ zad_2.3/src/com/company/User.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class User {
+    public static void kom(){
+    String login;
+    String password;
+        login = "abc" ;
+        password = "123";
+    }
+    class f extends User
+    {
+        public static void main(String[] args)
+        {
+            User.f arr_kyp[];
+        }
+    }
+}
